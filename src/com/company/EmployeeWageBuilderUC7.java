4af880538d6c92b04de134f70e737714a047a0c1
shinekumar23 <shinekumar23@gmail.com>
2020-08-19 00:05:03 +0530

    Compute employee wage using class Methods

--- src/com/company/EmployeeWageBuilderUC7.java
+++ src/com/company/EmployeeWageBuilderUC7.java
@@ -1,15 +1,13 @@
 package com.company;
 
-public class EmployeeWageBuilderUC6 {
+public class EmployeeWageBuilderUC7 {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NUM_WORKING_DAYS = 20;
     public static final int MAX_HRS_IN_MONTH=100;
-
-    public static void main(String[] args) {
+    public static int computeEmpWage(){
         int empHrs = 0;
-        int empWage = 0;
         int totalEmpHrs=0;
         int totalWorkingDays=0;
         while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS ){
@@ -30,5 +28,10 @@ public class EmployeeWageBuilderUC6 {
         }
         int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
         System.out.println("Total Employee Wage: " + totalEmpWage+"\nTotal Employee Hours: "+ totalEmpHrs);
+        return totalEmpWage;
+    }
+
+    public static void main(String[] args) {
+        computeEmpWage();
     }
 }

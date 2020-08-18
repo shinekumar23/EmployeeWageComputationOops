f8945213d59d4708945d80c54ba19dff67f07f02
shinekumar23 <shinekumar23@gmail.com>
2020-08-18 23:53:38 +0530

    Calculated wages till number of working days or total working hours is reached

--- src/com/company/EmployeeWageBuilderUC6.java
+++ src/com/company/EmployeeWageBuilderUC6.java
@@ -1,16 +1,19 @@
 package com.company;
 
-public class EmployeeWageBuilderUC5 {
+public class EmployeeWageBuilderUC6 {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NUM_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH=100;
 
     public static void main(String[] args) {
         int empHrs = 0;
         int empWage = 0;
-        int totalEmpWage = 0;
-        for (int day = 0; day <= NUM_WORKING_DAYS; day++) {
+        int totalEmpHrs=0;
+        int totalWorkingDays=0;
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS ){
+            totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case IS_PART_TIME:
@@ -22,10 +25,10 @@ public class EmployeeWageBuilderUC5 {
                 default:
                     empHrs = 0;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalEmpWage += empWage;
-            System.out.println("Emp Wage: " + empWage);
+            totalEmpHrs+=empHrs;
+            System.out.println("Day: " + totalWorkingDays + " Employee Hour: "+ empHrs);
         }
-        System.out.println("Total Employee Wage: " + totalEmpWage);
+        int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
+        System.out.println("Total Employee Wage: " + totalEmpWage+"\nTotal Employee Hours: "+ totalEmpHrs);
     }
 }
